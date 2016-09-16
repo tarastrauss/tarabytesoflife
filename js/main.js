c5dce3f14dd01d04ff571a980d26f9a672efd1ec
Tara Strauss <tara.m.strauss@gmail.com>
2016-09-16 14:20:13 -0700

    updates with python/joust and new resume

--- js/main.js
+++ js/main.js
@@ -1,41 +1,20 @@
-console.log('loaded!');
-
-
 $(document).ready(function(){
 
   $("#menu").vm_img();
 
   $('body').scrollspy({ target: '#menu', offset: 80 })
-  // $('#navbar-links').scrollspy();
 
-  // $('.button-margin a').click(function () {
-  //     $('.button-margin a').removeClass('active');
-  //     $(this).addClass('active');
-  //  });
+  $(document).on('click','.navbar-collapse.in',function(e) {
+      if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+          $(this).collapse('hide');
+      }
+  });
 
-// $('[data-spy="scroll"]').each(function () {
-//   var $spy = $(this).scrollspy('refresh')
-// })
-
-  // $('#menu').on('activate.bs.scrollspy', function () {
-  // // do something…
-  //       $('.button-margin a').removeClass('active');
-  //     $(this).addClass('active');
-  // })
-
-$(document).on('click','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
-        $(this).collapse('hide');
-    }
-});
-
- $special = $('.special');
+  $special = $('.special');
 
   $special.on('mouseover', function(event){
-    console.log('moused over');
     var $this = $(event.target);
-    var randomNum = Math.floor((Math.random() * 7) + 1);
-    console.log(randomNum);
+    var randomNum = Math.floor((Math.random() * 12) + 1);
     if ($this.html() === ''){
       switch(randomNum) {
         case 1: $this.append('<span class="label label-primary">CSS</span>').children().addClass('animated flipInX');
@@ -52,6 +31,16 @@ $(document).on('click','.navbar-collapse.in',function(e) {
           break;
         case 7: $this.append('<span class="label" style="background-color:#7D2CB2">Angular</span>').children().addClass('animated flipInX');
           break;
+        case 8: $this.append('<span class="label label-primary">React</span>').children().addClass('animated flipInX');
+          break;
+        case 9: $this.append('<span class="label label-success">Python</span>').children().addClass('animated flipInX');
+          break;
+        case 10: $this.append('<span class="label label-info">Django</span>').children().addClass('animated flipInX');
+          break;
+        case 11: $this.append('<span class="label label-danger">MySQL</span>').children().addClass('animated flipInX');
+          break;
+        case 12: $this.append('<span class="label label-warning">Selenium</span>').children().addClass('animated flipInX');
+          break;
       }
     }
     setTimeout(function(){
@@ -61,28 +50,5 @@ $(document).on('click','.navbar-collapse.in',function(e) {
        $this.html('');
       }, 300);
     }, 1100);
-    // });
   });
-// .animate({right: 20 }, 'slow')
-
-
-    // setInterval(function () {
-    //     $('.star-1').fadeOut(150).delay(2000).fadeIn(300).fadeOut(150).delay(1254);
-    //       $('.star-2').fadeOut(300).fadeIn(120).fadeOut(120).delay(1920);
-    //       $('.star-3').fadeOut(150).delay(1200).fadeIn(300).fadeOut(150).delay(800);
-    //       $('.star-4').fadeOut(700).fadeIn(300).fadeOut(160).delay(1350);
-    //   }, 1);
-    // });
-  // $column.on('mouseover', function(){
-  //   console.log('moused over');
-  //   $(this).fadeIn('slow');
-  //   // setTimeout(function(){
-  //   //   $this.html('');
-  //   //   console.log('hi!');
-  //   // }, 2000);
-  // });
 });
-
-
-    // <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.min.js"></script>
-    // <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.js"></script>
